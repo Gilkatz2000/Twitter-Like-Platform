c053ec6a3d79b76ca58651f98747c26a5b210c08
Gil Katz <gk2000@Gils-MacBook-Air.local>
2025-06-09 11:18:56 -0400

    worked on user.model and auth.controller

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import connectMongoDB from "./db/connectMongoDB.js";
+
+dotenv.config();
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;;
+
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
+    connectMongoDB();
 })

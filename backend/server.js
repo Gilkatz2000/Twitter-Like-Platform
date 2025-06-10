2c0baaf29cad0b148656354c10236d03631beb0b
Gil Katz <gk2000@Gils-Air.lan>
2025-06-10 08:42:04 -0400

    added protectRoute.js middleware

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,29 @@
 import express from "express";
-import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
 
-dotenv.config();
+import dotenv from "dotenv";
+import path from "path";
+
+// Load .env from backend folder when running from root
+dotenv.config({ path: path.resolve("backend/.env") });
+
 
 const app = express();
 const port = process.env.PORT || 8000;;
 
-app.use(express.json());
+app.get("/", (req,res) => {
+    res.send("Hello");
+})
 
-app.use("/api/auth", authRoutes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // ✅ first parse the body
+
+app.use(cookieParser());
+app.use("/api/auth", authRoutes); // ✅ then load the routes
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
     connectMongoDB();
-})
+});

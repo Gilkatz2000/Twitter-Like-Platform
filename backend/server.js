46980f8681f1aed2238a92080c2a87849483c2b6
Gil Katz <gk2000@Gils-Air.lan>
2025-06-23 22:05:02 -0400

    added user routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
 
@@ -22,6 +23,7 @@ app.use(express.urlencoded({ extended: true })); // ✅ first parse the body
 
 app.use(cookieParser());
 app.use("/api/auth", authRoutes); // ✅ then load the routes
+app.use("/api/users", userRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
